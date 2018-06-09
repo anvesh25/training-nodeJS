3e1d267e6e0ccdee9e036cc25e0ab3846be1923b
anvesh25 <babuanvesh.26@gmail.com>
2018-06-10 00:48:26 +0530

    commit

---
+++ BootStrapLearning/src/button.ts
@@ -0,0 +1,6 @@
+import * as jquery from 'jquery';
+//set button click event by using button id  
+jquery("#btn1").click(function(){
+     alert("You click default button");
+ });
+ //go and create file te config and webpack config
