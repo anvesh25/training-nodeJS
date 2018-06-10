84f1c714acebd3a728a677cd28f9a17932b7fde3
anvesh25 <babuanvesh.26@gmail.com>
2018-06-10 11:07:28 +0530

    commit

--- BootStrapLearning/src/button.ts
+++ BootStrapLearning/src/button.ts
@@ -3,4 +3,24 @@ import * as jquery from 'jquery';
 jquery("#btn1").click(function(){
      alert("You click default button");
  });
+
+ jquery("#btn2").click(function(){
+    alert("You click Primary button");
+});
+
+jquery("#btn3").click(function(){
+    alert("You click Info button");
+});
+
+jquery("#btn4").click(function(){
+    alert("You click Warning button");
+});
+
+jquery("#btn5").click(function(){
+    alert("You click Succress button");
+});
+
+jquery("#btn6").click(function(){
+    alert("You click Danger button");
+});
  //go and create file te config and webpack config
