73565cb138d49db4ca97157664d5fd2257a25ac9
anvesh25 <babuanvesh.26@gmail.com>
2018-06-10 11:33:25 +0530

    commit

--- BootStrapLearning/src/button.ts
+++ BootStrapLearning/src/button.ts
@@ -1,26 +1,26 @@
-import * as jquery from 'jquery';
+import * as jQuery from 'jquery';
 //set button click event by using button id  
-jquery("#btn1").click(function(){
+jQuery("#btn1").click(function(){
      alert("You click default button");
  });
 
- jquery("#btn2").click(function(){
+ jQuery("#btn2").click(function(){
     alert("You click Primary button");
 });
 
-jquery("#btn3").click(function(){
+jQuery("#btn3").click(function(){
     alert("You click Info button");
 });
 
-jquery("#btn4").click(function(){
+jQuery("#btn4").click(function(){
     alert("You click Warning button");
 });
 
-jquery("#btn5").click(function(){
+jQuery("#btn5").click(function(){
     alert("You click Succress button");
 });
 
-jquery("#btn6").click(function(){
+jQuery("#btn6").click(function(){
     alert("You click Danger button");
 });
  //go and create file te config and webpack config
--- BootStrapLearning/webpack.config.js
+++ BootStrapLearning/webpack.config.js
@@ -1,26 +1,29 @@
+const path = require('path');
 module.exports = {
     entry: "./src/button.ts",
-    mode: "development",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
-
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".js", ".json"]
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
     },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.ts?$/, loader: "awesome-typescript-loader" },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js"]
+    },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
+    output: 
+    {
+        filename: 'bondle.js',
+        path: path.resolve(__dirname, 'dist')
     }
 };
